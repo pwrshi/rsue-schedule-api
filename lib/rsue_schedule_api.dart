73398804d9c192b279d8abe7b47618c2b91f8685
pwrshi <stasonukesweet@gmail.com>
2022-05-02 23:03:03 +0300

    Map to List

--- lib/rsue_schedule_api.dart
+++ lib/rsue_schedule_api.dart
@@ -9,3 +9,9 @@ export 'get_schedule_raw_data.dart';
 export 'get_schedule.dart';
 
 export 'get_all_schedules.dart';
+
+export 'get_all_groups.dart';
+
+export 'get_facults.dart';
+
+export 'schedule_type.dart';
