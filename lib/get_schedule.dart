07223f7cfba77272a6afbeddcd787af067636f86
tashinuke <stasonukesweet@gmail.com>
2023-03-15 09:43:39 +0300

    fix parse func

--- lib/get_schedule.dart
+++ lib/get_schedule.dart
@@ -16,10 +16,69 @@ import 'package:rsue_schedule_api/rsue_schedule_api.dart';
 ///   }
 /// }
 /// ```
-Future<ScheduleService?> getSchedule(int faculty, int course, int group) async {
-  var scheduleRaw = await getScheduleRawData(faculty, course, group);
-  if (scheduleRaw != null) {
-    return ScheduleService.fromJson(scheduleRaw);
+///
+///
+DayOfWeek _nameToDayOfWeek(String weekdayName) {
+  switch (weekdayName) {
+    case "Понедельник":
+      return 1;
+    case "Вторник":
+      return 2;
+    case "Среда":
+      return 3;
+    case "Четверг":
+      return 4;
+    case "Пятница":
+      return 5;
+    case "Суббота":
+      return 6;
+    case "Воскресенье":
+      return 7;
+    default:
+      return -1;
   }
-  return null;
+}
+
+Map<DayOfWeek, List<AbstractLesson>> _parseWeek(
+    Map<String, List<Map<String, String>>> weekRaw) {
+  Map<DayOfWeek, List<AbstractLesson>> result = {};
+  for (var dayRaw in weekRaw.entries) {
+    result[_nameToDayOfWeek(dayRaw.key)] = dayRaw.value.map((element) {
+      LessonType type;
+      switch (element["type"]) {
+        case "Лекция":
+          type = LessonType.lection;
+          break;
+        case "Практика":
+          type = LessonType.practice;
+          break;
+        default:
+          type = LessonType.lab;
+      }
+      return AbstractLesson(
+          name: element["name"] ?? "",
+          teachersname: element["teacher"] ?? "",
+          room: element["room"] ?? "",
+          type: type,
+          time: element["time"] ?? "");
+    }).toList();
+  }
+  return result;
+}
+
+Future<ScheduleService?> getSchedule(int faculty, int course, int group) async {
+  var raw = await getScheduleRawData(faculty, course, group);
+  Map<String, List<Map<String, String>>> evenRaw;
+  Map<String, List<Map<String, String>>> oddRaw;
+  try {
+    oddRaw = raw!["Нечетная неделя"]!;
+  } catch (e) {
+    throw Exception('"Нечетная неделя" не найдена');
+  }
+  try {
+    evenRaw = raw["Четная неделя"]!;
+  } catch (e) {
+    throw Exception('"Четная неделя" не найдена');
+  }
+  return ScheduleService(_parseWeek(evenRaw), _parseWeek(oddRaw));
 }
